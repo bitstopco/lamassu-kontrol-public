1fed5a96b80970ebaa63a9863221582f251fb8df
Lazaro Herrera <lazherrera@gmail.com>
2016-05-18 17:03:03 -0400

    Initial Code Injection for Lamassu Devices

--- kontrol.js
+++ kontrol.js
@@ -1,6 +1,8 @@
 require('dotenv').load();
 
 var minutes = 5;
+var root_path = "/Users/lazaroherrera/lamassu-machine-original/"
+
 var os = require('os');
 var address = require('network-address');
 var fs = require('fs');
@@ -13,25 +15,23 @@ var Firebase = require("firebase");
 var Pusher = require('pusher-client');
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
+
 exec('cd /var/lib/lamassu-machine && openssl x509 -noout -in client.pem -fingerprint',
   function (error, stdout, stderr) {
     global.fingerprint = stdout.replace("SHA1 Fingerprint=", "").trim().split(":").join("");
     
-    if(process.env.token != undefined)
-    {
+    if(process.env.BITSTOP_TOKEN == undefined) {
       //Query for the new .env file (API)
       var options = {
         headers: {
           'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Bitstop-Token': process.env.token
+          'Accept': 'application/json'
         },
         json: true
       }
 
       var body ={
-        fingerprint: fingerprint,
-        token: process.env.token
+        fingerprint: fingerprint
       }
 
       needle.post("https://"+process.env.url+"/api/first_time", body, options, function(err, resp){
@@ -39,7 +39,7 @@ exec('cd /var/lib/lamassu-machine && openssl x509 -noout -in client.pem -fingerp
           var re_data = resp.body;
           var final_env = "";
           for(var attributename in re_data){
-            final_env += attributename+"='"+re_data[attributename]+"'\n";
+            final_env += attributename+"="+re_data[attributename]+"\n";
           }
 
           fs.renameSync(path.resolve(__dirname, '.env'),path.resolve(__dirname, '.env-old'))
@@ -90,26 +90,94 @@ function send_logs() {
   });
 }
 
+
+function backup_lamassu_files(){
+  try {
+    fs.statSync('./backup')
+    return false;
+  }
+  catch (e) {
+    console.log("BACKUP STARTED")
+    fs.mkdirSync('./backup')
+    exec('cp -R ' + root_path + " " + './backup/lamassu-machine/', function(error, stdout, stderr){
+      inject_lamassu_receipts()
+    })
+    return true;
+  }
+}
+
+function restore_backup(){
+  try{
+    fs.statSync('./backup')
+    console.log("RESTORE BACKUP")
+    exec('cp -R ' + './backup/lamassu-machine/' + " " + root_path)
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+function inject_code(src, target, match, before, replace){
+  var injected_data = fs.readFileSync(src, 'utf8');
+  var target_data = fs.readFileSync(target, 'utf8');
+
+  console.log('INJECTING ' + src)
+  if (replace){
+    target_data = target_data.replace(match, injected_data)
+  }
+  else if (before) {
+    target_data = target_data.replace(match, injected_data +  "\n" + match)
+  } else {
+    target_data = target_data.replace(match, match +  "\n" + injected_data)
+  }
+
+  fs.writeFileSync(target, target_data)
+}
+
+function inject_lamassu_receipts(){
+  console.log("INSTALLING DEPENDENCIES")
+  //Install files
+  exec("cd " + root_path + " && npm install needle --save")
+  exec("cd " + root_path + " && npm install dotenv --save")
+  
+  console.log("PUTTING ENV VARIABLES")
+  //Dump env vars
+  fs.writeFileSync(root_path + '.env', "BITSTOP_TOKEN=" + process.env.BITSTOP_TOKEN + "\n" + "FINGERPRINT=" +  fingerprint);
+
+  console.log("INJECTING CODE NOW")
+  //Inject files
+  inject_code('./partials/email_receipt/app.js.1', root_path + "ui/js/app.js", "var phoneKeypad = null", true, false)
+  inject_code('./partials/email_receipt/app.js.2', root_path + "ui/js/app.js", "  wifiKeyboard = new Keyboard('wifi-keyboard').init()", false, false)
+  inject_code('./partials/email_receipt/app.js.3', root_path + "ui/js/app.js", "  setupButton('initialize', 'initialize')", true, false)
+  inject_code('./partials/email_receipt/app.js.4', root_path + "ui/js/app.js", "  var languageButtons = document.getElementById('languages')", true, false)
+  inject_code('./partials/email_receipt/app.js.5', root_path + "ui/js/app.js", "  wifiKeyboard.reset()", false, false)
+
+  inject_code('./partials/email_receipt/brain.js.1', root_path + "lib/brain.js", "var fs = require('fs')", true, false)
+  inject_code('./partials/email_receipt/brain.js.2', root_path + "lib/brain.js", "      this._chooseFiat()", true, false)
+  inject_code('./partials/email_receipt/brain.js.3', root_path + "lib/brain.js", "  this._startAddressScan()", true, true)
+  inject_code('./partials/email_receipt/brain.js.4', root_path + "lib/brain.js", "  this._setState('completed')", false, false)
+
+  inject_code('./partials/email_receipt/start.html.1', root_path + "ui/start.html", '      <section class="viewport idle_state" data-tr-section="idle">', true, false)
+}
+
 function check_for_commands(data) {
   console.log("Checking for commands - " + dateFromNum(Date.now()));
 
   var re_data = data;
-  switch(re_data.command) {
-    case 'restart-machine':
-      // reboot machine
-      console.log('Restarting Machine');
-      exec('sudo shutdown -c');
-      exec('sudo reboot');
+  switch(re_data.command){
+    case 'inject_email_receipt':
+      //Backup lamassu files
+      //Email receipts injection
+      console.log("BEGIN CODE INJECTION ATTEMPT")
+      backup_lamassu_files()
       break;
-    case 'restart-lamassu-machine':
-      // reboot lamassu-machine process
-      console.log('Restarting lamassu-machine');
-      exec('restart lamassu-machine');
+    case 'undo_inject_email_receipt':
+      restore_backup()
       break;
     case 'update':
       // update code from githug repo
       console.log('Updating lamassu-machine');
-      exec('cd /opt/apps/machine/lamassu-machine && git pull && reboot');
+      exec('git pull && reboot');
       break;
     }
 }
